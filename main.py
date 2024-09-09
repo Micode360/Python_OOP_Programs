b38f6bce4e5934cdfafa314c1ff8f2bd2ef4b1ff
friday123kelvin <fridaykelvin69@gmail.com>
2024-09-09 15:58:59 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from mirabank.index import MiraBank
+from Yourservice import Kp_montana
+
 class ServiceManager:
     def __init__(self):
         self.mira_bank = MiraBank()
+        self.kp_montana = Kp_montana()
         
     def run(self):
         while True:
             print('Hello welcome to  mi-global network. What service do you like to use?')
-            print('1. Mirabank\n2. Exit')
+            print('1. Mirabank\n2. Kp_montana\n8. Exit')
             choice = input("Choose 1/2: ")
 
             if choice == "1":
                 self.mira_bank.run()
-            if choice == "2":
+            if choice == "7":
+                self.kp_montana.run()
+            if choice == "8":
                 print("thank you for choosing our services")
                 break
             else:
